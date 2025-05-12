f4a7a66d90588fce119221777b2ac8a660952734
Simran <drsimran69@gmail.com>
2025-05-13 00:04:34 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -20,9 +20,4 @@ def error_message_detail(error,error_detail:sys):
         
         
 
-            if __name__=="__main__":
-                try:
-                    a=1/0
-                except Exception as e:
-                    logging.info("Divide by zero error")
-                    raise CustomException
+           
