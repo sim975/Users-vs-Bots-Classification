d26c6147ce57d7a27e4182a33be7988f856bff7d
Simran <drsimran69@gmail.com>
2025-05-13 17:05:10 +0530

    Data ingestion

--- src/logger.py
+++ src/logger.py
@@ -2,23 +2,22 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Create a logs directory
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
 
+# Create a unique log file name with timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
-
-
+# Set up logging configuration
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format=
-    "%(asctime)s: %(levelname)s: %(message)s",  
-
+    format="%(asctime)s: %(levelname)s: %(message)s",
     level=logging.INFO,
 )
 
-if __name__=="__main__":
+if __name__ == "__main__":
     logging.info("Logging has started")
     logging.info("Logging has ended")
 
